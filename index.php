21979fcc83bf628ede50a3d9f66ec5dee12bfdf4
Imran Hossain <umemous@gmail.com>
2019-05-06 18:03:46 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <style>
+  
   .video-container {
-position: relative;
-padding-bottom: 56.25%;
-padding-top: 30px; height: 0; overflow: hidden;
-}
-
-.video-container iframe,
-.video-container object,
-.video-container embed {
-position: absolute;
-top: 0;
-left: 0;
-width: 100%;
-height: 100%;
-}
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px; height: 0; overflow: hidden;
+  }
+  .video-container iframe,
+  .video-container object,
+  .video-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  
 </style>
 
-
 <div class="video-container">
-<iframe src="https://www.youtube.com/embed/z9Ul9ccDOqE" width="853" height="480" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+  <iframe src="https://www.youtube.com/embed/z9Ul9ccDOqE" width="853" height="480" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
 </div>
